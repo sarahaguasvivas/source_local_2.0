0b8d002d7162d34ec2fbff926e912a0cf6b424f8
sarahaguasvivas <saag5228@colorado.edu>
2018-10-24 13:56:00 -0600

    fast collection code

---
+++ plotData.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+import struct
+#df= pd.read_csv('testDataTest.csv')
+#plt.plot(df.iloc[:, 0])
+#plt.plot(df.iloc[:, 1])
+#plt.plot(df.iloc[:, 2])
+
+data= open('testDataTest.txt', 'r')
+data= data.read().replace('\n', '')
+str1= str(len(data)/4) + "f"
+data= str(struct.unpack(str1, data))  
+print(data)
+list_data= data.split(')(')
+data= {}
+dataData= []
+
+for i in range(len(list_data)):
+	list_data[i]= list_data[i].replace('(','' )
+	list_data[i]= list_data[i].replace(')', '')
+	data[i]= list_data[i].split(',')
+	if len(data[i]) > 0:
+		data[i]= [float(j) for j in data[i]]
+		dataData+= data[i]
+
+plt.scatter(range(len(dataData)), [float(i) for i in dataData])
+plt.show()
+data= np.reshape(dataData, (-1, 2))
+print(data.shape)
+plt.plot(range(data.shape[0]), data[:, 0])
+plt.plot(range(data.shape[0]), data[:, 1])
+plt.show()
+"""
+df= pd.read_csv('testData2.csv')
+plt.plot(df.iloc[:, 0])
+plt.plot(df.iloc[:, 1])
+plt.plot(df.iloc[:, 2])
+
+plt.show()
+
+df= pd.read_csv('testData3.csv')
+plt.plot(df.iloc[:, 0])
+plt.plot(df.iloc[:, 1])
+plt.plot(df.iloc[:, 2])
+
+plt.show()
+
+df= pd.read_csv('testData4.csv')
+plt.plot(df.iloc[:, 0])
+plt.plot(df.iloc[:, 1])
+plt.plot(df.iloc[:, 2])
+
+plt.show()
+"""
