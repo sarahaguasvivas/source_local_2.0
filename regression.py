0594c05fcb7a05796fd067ef26819424447f9fde
sarahaguasvivas <saag5228@colorado.edu>
2018-11-12 17:05:00 -0700

    regression module

--- regression.py
+++ regression.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
-from __future__ import absolute_import
-from __future__ import division
 from __future__ import print_function
-
-import tensorflow as tf
-import numpy as np
+from keras.layers import Dense, Activation
+from keras.models import Sequential
+from sklearn import preprocessing
 import pandas as pd
+import numpy as np
 import random
-
-def init_weights(shape):
-    return tf.Variable(tf.random_normal(shape, stddev=0.01))
-
-def model(X, w_h, w_o):
-    h= tf.nn.sigmoid(tf.matmul(X, w_h))
-    return tf.matmul(h, w_o)
-
+def model_function(data, labels, test, lab_test):
+    model= Sequential()
+    model.add(Dense(input_dim=250, output_dim=500))
+    model.add(Activation('relu'))
+    model.add(Dense(input_dim=500, output_dim=2))
+    model.compile(loss='mean_squared_error', optimizer='rmsprop')
+    model.fit(data, labels, batch_size=1, nb_epoch=10,  verbose=2, validation_data=(test, lab_test))
+    print(model.predict(test, batch_size=1))
+    return model
 if __name__== '__main__':
     train=0.8
     WINDOW_SIZE=125
@@ -29,9 +29,10 @@ if __name__== '__main__':
     missing = list(set(ind)- set(indexes))
 
     training_data= data[indexes, data.shape[1]-2]
+    print(training_data.shape[1])
     testing_data = data[missing, data.shape[1]-2]
     training_labels= data[indexes, -2:]
     testing_labels= data[missing, -2:]
-    print(training_data.shape)
-    X= tf.placeholder("float", [None, ])
+    model= model_function(training_data, training_labels, testing_data, testing_labels)
+
 
