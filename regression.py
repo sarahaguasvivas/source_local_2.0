3c8c86002b8432ddceea894f6101bcbf179c2654
Sarah Aguasvivas <sarahaguasvivas@Sarahs-MacBook-Pro.local>
2018-11-23 12:46:29 -0700

    convolution layer

--- regression.py
+++ regression.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 
 from __future__ import print_function
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, Conv1D, MaxPooling1D, Flatten
 from keras.models import Sequential
+from keras.losses import cosine_proximity, mean_squared_error
 from sklearn import preprocessing
 import pandas as pd
 import numpy as np
@@ -14,19 +15,24 @@ from sklearn.svm import SVR
 import matplotlib.pyplot as plt
 from scipy import signal
 WAVELET_THRESHOLD=0.1
+
+def custom_loss(y_true, y_pred):
+    #return cosine_proximity(y_true, y_pred)
+    return mean_squared_error(y_true, y_pred)
 def custom_activation(x):
-    return K.tanh(x)
+    return K.sigmoid(x)
 
 def model_function(data, labels, test, lab_test):
+    data= data.reshape(data.shape[0], data.shape[1], 1)
+    test= test.reshape(test.shape[0], test.shape[1], 1)
     model= Sequential()
-    model.add(Dense(input_dim=250*30, output_dim=500))
-    model.add(Activation(custom_activation))
-    model.add(Dense(input_dim=500, output_dim=300))
-    model.add(Activation(custom_activation))
-    model.add(Dense(input_dim=300, output_dim= 1000))
-    model.add(Activation(custom_activation))
-    model.add(Dense(input_dim=1000, output_dim= 2))
-    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.add(Conv1D(filters=32, kernel_size=3, input_shape=(250, 1)))
+    model.add(MaxPooling1D(pool_size=5))
+    model.add(Flatten())
+    model.add(Dense(input_dim= 1000, output_dim=500, activation=custom_activation))
+    model.add(Dense(input_dim=500, output_dim=1 , activation=None))
+
+    model.compile(loss=custom_loss, optimizer='adam')
     model.fit(data, labels, batch_size=1, nb_epoch=100,  verbose=2, validation_data=(test, lab_test))
     print(model.predict(test, batch_size=1))
     print("Labels:")
@@ -50,9 +56,9 @@ if __name__== '__main__':
     testing_data1 = data[missing, :data.shape[1]-2]
     training_labels1= data[indexes, -2:]
     training_labels= np.empty((0, 2))
-    training_data= np.empty((0, 250*30))
+    training_data= np.empty((0, 250))
     testing_labels= np.empty((0, 2))
-    testing_data= np.empty((0, 250*30))
+    testing_data= np.empty((0, 250))
     testing_labels1= data[missing, -2:]
 
     for i in range(training_data1.shape[0]):
@@ -60,15 +66,15 @@ if __name__== '__main__':
         cwtmatr= signal.cwt(training_data1[i, :], signal.ricker, widths)
         if np.max(np.abs(cwtmatr))>=WAVELET_THRESHOLD:
             training_labels= np.vstack((training_labels, training_labels1[i, :]))
-            training_data= np.vstack((training_data, cwtmatr.reshape(1, -1)))
+            training_data= np.vstack((training_data, training_data1[i, :]))
 
     for i in range(testing_data1.shape[0]):
         widths= np.arange(1, 31)
         cwtmatr1= signal.cwt(testing_data1[i, :], signal.ricker, widths)
         if np.max(np.abs(cwtmatr1))>=WAVELET_THRESHOLD:
             testing_labels= np.vstack((testing_labels, testing_labels1[i, :]))
-            testing_data= np.vstack((testing_data, cwtmatr1.reshape(1, -1)))
+            testing_data= np.vstack((testing_data, testing_data1[i, :]))
 
-    model= model_function(training_data, training_labels, testing_data, testing_labels)
+    model= model_function(training_data, training_labels[:, 1], testing_data, testing_labels[:, 1])
     print(testing_labels.shape)
     print(testing_data.shape)
