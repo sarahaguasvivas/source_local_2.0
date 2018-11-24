1d5e152bafbbee34a7c617e7cae3d4a6d19bfc54
Sarah Aguasvivas <sarahaguasvivas@Sarahs-MacBook-Pro.local>
2018-11-23 18:37:33 -0700

    normalized labels

--- regression.py
+++ regression.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python2
 
 from __future__ import print_function
-from keras.layers import Dense, Activation, Conv1D, MaxPooling1D, Flatten
+from keras.layers import Dense, Activation, Conv1D, MaxPooling1D, Flatten, Dropout
 from keras.models import Sequential
 from keras.losses import cosine_proximity, mean_squared_error
 from sklearn import preprocessing
+from keras.activations import exponential, linear
 import pandas as pd
 import numpy as np
+from tensorflow.losses import huber_loss
 import random
 from keras import backend as K
 import sys
@@ -15,30 +17,50 @@ from sklearn.svm import SVR
 import matplotlib.pyplot as plt
 from scipy import signal
 WAVELET_THRESHOLD=0.1
-
+def plot_history(history):
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Mean Abs Error [1000$]')
+    plt.plot(history.epoch, np.array(history.history['mean_absolute_error']),
+           label='Train Loss')
+    plt.plot(history.epoch, np.array(history.history['val_mean_absolute_error']),
+           label = 'Val loss')
+    plt.legend()
+    plt.ylim([0, 5])
+    plt.show()
 def custom_loss(y_true, y_pred):
+    #return huber_loss(y_true, y_pred)
+    return 100*180*mean_squared_error(y_true, y_pred)
     #return cosine_proximity(y_true, y_pred)
-    return mean_squared_error(y_true, y_pred)
+
 def custom_activation(x):
-    return K.sigmoid(x)
+    return K.sigmoid(x)-0.5
+    #return K.tanh(x)
+    #return exponential(x)
+    #return 10*linear(x)
 
 def model_function(data, labels, test, lab_test):
-    data= data.reshape(data.shape[0], data.shape[1], 1)
-    test= test.reshape(test.shape[0], test.shape[1], 1)
-    model= Sequential()
-    model.add(Conv1D(filters=32, kernel_size=3, input_shape=(250, 1)))
-    model.add(MaxPooling1D(pool_size=5))
-    model.add(Flatten())
-    model.add(Dense(input_dim= 1000, output_dim=500, activation=custom_activation))
-    model.add(Dense(input_dim=500, output_dim=1 , activation=None))
-
-    model.compile(loss=custom_loss, optimizer='adam')
-    model.fit(data, labels, batch_size=1, nb_epoch=100,  verbose=2, validation_data=(test, lab_test))
-    print(model.predict(test, batch_size=1))
-    print("Labels:")
     print(lab_test)
-    return model
+    data= data.reshape(data.shape[0], data.shape[1], 1).astype(np.float)
+    test= test.reshape(test.shape[0], test.shape[1], 1).astype(np.float)
 
+    model= Sequential()
+    model.add(Conv1D(filters=64, kernel_size=5, activation='relu', input_shape=(250, 1)))
+    model.add(MaxPooling1D(pool_size=5))
+
+    model.add(Flatten())
+    model.add(Dense(input_dim= 4000, output_dim=10, activation=custom_activation))
+    model.add(Dense(input_dim= 25, output_dim=2, activation=custom_activation))
+    model.add(Dropout(0.3))
+    model.compile(loss=custom_loss, optimizer='adam')
+    print(lab_test)
+    print('here')
+    history= model.fit(data, labels, batch_size=10, nb_epoch=500,  verbose=2, validation_data=(test, lab_test))
+    predictions=model.predict(test, batch_size=1)
+    print(predictions[:, 0]*180)
+    print(lab_test[:, 0]*180)
+    print(predictions[:, 1]*100)
+    print(lab_test[:, 1]*100)
 if __name__== '__main__':
     train=0.8
     WINDOW_SIZE=125
@@ -60,7 +82,6 @@ if __name__== '__main__':
     testing_labels= np.empty((0, 2))
     testing_data= np.empty((0, 250))
     testing_labels1= data[missing, -2:]
-
     for i in range(training_data1.shape[0]):
         widths= np.arange(1, 31)
         cwtmatr= signal.cwt(training_data1[i, :], signal.ricker, widths)
@@ -74,7 +95,8 @@ if __name__== '__main__':
         if np.max(np.abs(cwtmatr1))>=WAVELET_THRESHOLD:
             testing_labels= np.vstack((testing_labels, testing_labels1[i, :]))
             testing_data= np.vstack((testing_data, testing_data1[i, :]))
-
-    model= model_function(training_data, training_labels[:, 1], testing_data, testing_labels[:, 1])
-    print(testing_labels.shape)
-    print(testing_data.shape)
+    training_labels[:, 0]= training_labels[:, 0]/180
+    testing_labels[:, 0]= testing_labels[:,0]/180
+    training_labels[:, 1]= training_labels[:, 1]/100
+    testing_labels[:, 1]= testing_labels[:, 1]/100
+    model= model_function(training_data, training_labels, testing_data, testing_labels)
