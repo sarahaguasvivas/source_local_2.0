83a05476462b03e0afd98c9b1286109b2041a80b
Sarah Aguasvivas <sarahaguasvivas@Sarahs-MacBook-Pro.local>
2018-11-25 16:42:11 -0700

    saving weights now

--- regression.py
+++ regression.py
@@ -31,26 +31,20 @@ def plot_history(history):
     plt.ylim([0, 5])
     plt.show()
 def custom_loss(y_true, y_pred):
-    #return huber_loss(y_true, y_pred)
-    #return mean_squared_error(y_true, y_pred)
-    #return cosine_proximity(y_true, y_pred)
-    #y_true= y_true.eval(session=sess)
-    #y_pred= y_pred.eval(session=sess)
+   # return mean_squared_error(y_true, y_pred)
     a= y_true[:, 1]
     b= y_pred[:, 1]
     d= y_true[:, 0]
     e= y_pred[:, 0]
-    delthet=d-e
+    delthet=e-d
     cosdelt= K.cos(delthet*np.pi/180)
 
-    c = K.abs(K.square(a) + K.square(b) - 2*a*b*cosdelt)
+    c =K.abs(K.square(a) + K.square(b) - 2*a*b*cosdelt)
     res = K.mean(c)
-    return res;
-    #c= np.sqrt(a**2+ b**2 - 2*a*b*cosdelt)
-    #return tf.convert_to_tensor(np.max(c), dtype= tf.float64)
+    return res
 
 def custom_activation(x):
-    #return 30*K.sigmoid(x)
+    #return K.sigmoid(x)-0.5
     return 10*K.tanh(x)
     #return exponential(x)
     #return 10*linear(x)
@@ -61,21 +55,25 @@ def model_function(data, labels, test, lab_test):
     test= test.reshape(test.shape[0], test.shape[1], 1)
 
     model= Sequential()
-    model.add(Conv1D(filters=32, kernel_size=5, input_shape=(250, 1)))
+    model.add(Conv1D(filters=125, kernel_size=5, input_shape=(250, 1)))
+    model.add(Conv1D(filters=64, kernel_size=5))
+
     model.add(MaxPooling1D(5))
     model.add(Flatten())
     model.add(BatchNormalization())
-    model.add(Dense(input_dim= 32*250, output_dim=3000, activation=custom_activation))
-    model.add(Dense(input_dim= 3000, output_dim=2, activation=None))
+    model.add(Dense(output_dim=256,input_dim=250*32, activation=custom_activation))
+    model.add(Dense(2, activation=None))
 
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.9))
     model.compile(loss=custom_loss, optimizer='rmsprop')
     history= model.fit(data, labels, batch_size=10, nb_epoch=1000,  verbose=2, validation_data=(test, lab_test))
     predictions=model.predict(test, batch_size=1)
     print(predictions)
     print(lab_test)
+    model.save_weights('weights.h5py')
+
 if __name__== '__main__':
-    train=0.85
+    train=0.7
     WINDOW_SIZE=125
     NUM_ADC=2
     data= pd.read_csv('alldata.csv').values
@@ -109,9 +107,9 @@ if __name__== '__main__':
             testing_labels= np.vstack((testing_labels, testing_labels1[i, :]))
             testing_data= np.vstack((testing_data, testing_data1[i, :]))
 
-    testing_labels[:, 0]= testing_labels[:,0]/180
-    training_labels[:, 1]= training_labels[:, 1]/100
-    testing_labels[:, 1]= testing_labels[:, 1]/100
-    training_labels[:, 0]= training_labels[:, 0]/180
+    testing_labels[:, 0]= testing_labels[:,0]
+    training_labels[:, 1]= training_labels[:, 1]
+    testing_labels[:, 1]= testing_labels[:, 1]
+    training_labels[:, 0]= training_labels[:, 0]
 
     model= model_function(training_data, training_labels, testing_data, testing_labels)
