62e48fc17ca7d7c024e2a14b47166e50b371c4fd
Sarah Aguasvivas <sarahaguasvivas@Sarahs-MacBook-Pro.local>
2018-11-24 22:33:57 -0700

    new cost function

--- regression.py
+++ regression.py
@@ -17,7 +17,8 @@ from sklearn.svm import SVR
 import matplotlib.pyplot as plt
 from scipy import signal
 WAVELET_THRESHOLD=0.1
-
+sess = tf.Session()
+K.set_session(sess)
 def plot_history(history):
     plt.figure()
     plt.xlabel('Epoch')
@@ -30,9 +31,23 @@ def plot_history(history):
     plt.ylim([0, 5])
     plt.show()
 def custom_loss(y_true, y_pred):
-    return huber_loss(y_true, y_pred)
+    #return huber_loss(y_true, y_pred)
     #return mean_squared_error(y_true, y_pred)
     #return cosine_proximity(y_true, y_pred)
+    #y_true= y_true.eval(session=sess)
+    #y_pred= y_pred.eval(session=sess)
+    a= y_true[:, 1]
+    b= y_pred[:, 1]
+    d= y_true[:, 0]
+    e= y_pred[:, 0]
+    delthet=d-e
+    cosdelt= K.cos(delthet*np.pi/180)
+
+    c = K.abs(K.square(a) + K.square(b) - 2*a*b*cosdelt)
+    res = K.mean(c)
+    return res;
+    #c= np.sqrt(a**2+ b**2 - 2*a*b*cosdelt)
+    #return tf.convert_to_tensor(np.max(c), dtype= tf.float64)
 
 def custom_activation(x):
     #return 30*K.sigmoid(x)
@@ -52,12 +67,10 @@ def model_function(data, labels, test, lab_test):
     model.add(BatchNormalization())
     model.add(Dense(input_dim= 32*250, output_dim=3000, activation=custom_activation))
     model.add(Dense(input_dim= 3000, output_dim=2, activation=None))
-    #model.add(Dense(input_dim= 1000, output_dim=300, activation=None))
-    #model.add(Dense(input_dim= 300, output_dim=2, activation=None))
 
     model.add(Dropout(0.5))
     model.compile(loss=custom_loss, optimizer='rmsprop')
-    history= model.fit(data, labels, batch_size=10, nb_epoch=100,  verbose=2, validation_data=(test, lab_test))
+    history= model.fit(data, labels, batch_size=10, nb_epoch=1000,  verbose=2, validation_data=(test, lab_test))
     predictions=model.predict(test, batch_size=1)
     print(predictions)
     print(lab_test)
